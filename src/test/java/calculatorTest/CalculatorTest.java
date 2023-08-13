95b6d70fbc5382f36732a9a6917aa3a3dbfb0ba1
EraseDa <fkakrla123@gmail.com>
2023-08-13 22:47:52 +0900

    calculator 연습

---
+++ src/test/java/calculatorTest/CalculatorTest.java
@@ -0,0 +1,38 @@
+package calculatorTest;
+
+import calculator.Calculator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    private Calculator cal;
+
+    @BeforeEach
+    public void setup() {
+        cal = new Calculator();
+        System.out.println("before");
+    }
+    @Test
+    @DisplayName("더하기")
+    public void add() {
+        assertEquals(9, cal.add(3,6));
+        System.out.println("add");
+    }
+
+    @Test
+    @DisplayName("빼기")
+    public void subtract() {
+        assertEquals(6, cal.subtract(7,1));
+        System.out.println("subtract");
+    }
+
+    @AfterEach
+    public void teardown() {
+        System.out.println("teardown");
+    }
+}
